e59ea50d6494db60b55bc95f5e57a70134f39c3a
tnn1t1s <palaitis@davids-air.home>
2015-08-09 00:42:53 -0400

    last time ill ever do git add .

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to learn-riemann
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/learn_riemann/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns learn-riemann.core-test
-  (:require [clojure.test :refer :all]
-            [learn-riemann.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
