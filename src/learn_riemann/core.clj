9f98a85a0ba3b8e645921e9ee75c8fdfbf6754ac
tnn1t1s <palaitis@Davids-MacBook-Air.local>
2015-08-10 08:56:12 -0400

    expired, stable

--- src/learn_riemann/core.clj
+++ src/learn_riemann/core.clj
@@ -12,11 +12,13 @@
 
 ; send your first event
 (riemann-send c {:service "dz0" :state "ok" :metric 90.0})
+(riemann-send c {:service "dz0" :state "error" :metric 90.0})
 
 ; query the index for the event
 @(r/query c "service = \"/xxx/yyy\"")
 @(r/query c "service = \"dz0-mean\"")
-@(r/query c "service = \"dz0.*\"")
+@(r/query c "service = \"dz0\"")
+@(r/query c "state = \"ok\"")
 
 
 ; testing the where stream
@@ -42,8 +44,9 @@
 (def x [5.0, 5.0, 5.0, 4.0, 6.0, 5.0, 8.0, 9.0, 7.0, 7.0, 7.0, 7.0, 8.0, 6.0, 7.0, 5.0, 7.0, 6.0, 9.0, 3.0, 6.0])
 (def y (take 20 (repeatedly #(rand-int 10))))
 (def z (take 5 (repeatedly #(int 1))))
+; test expired events
 (map #(do
         (riemann-send c {:service "dz0" :state "ok" :metric %})
-        (Thread/sleep 1000)) y)
+        (Thread/sleep 1000)) z)
 
 
