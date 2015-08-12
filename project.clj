44b1a7187e66630041d13c6e4075543ee6308b07
tnn1t1s <palaitis@Davids-MacBook-Air.local>
2015-08-12 08:37:27 -0400

    added jarohen/chime

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
                  [riemann-clojure-client "0.4.1"]
+                 [jarohen/chime "0.1.6"]
                  [org.clojure/clojure "1.6.0"]])
