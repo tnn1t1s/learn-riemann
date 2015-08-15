fcbe6cf2a4df9c0787281a196e9be4303e9488f7
tnn1t1s <palaitis@davids-air.home>
2015-08-15 09:48:06 -0400

    coalesce

--- src/learn_riemann/learn.clj
+++ src/learn_riemann/learn.clj
@@ -1,12 +1,14 @@
 (ns learn-riemann.learn
       (:require
         [riemann.client :as r]
-        [chime :refer [chime-at]]
+        [chime :refer [chime-at chime-ch]]
         [clj-time.core :as t]
         [clj-time.periodic :refer [periodic-seq]]
         [incanter.core :as incanter]
+        [clojure.core.async :as a :refer [<! go-loop]]
         [incanter.charts :as charts]
-        ))
+        )
+      (:use [incanter core stats charts io]))
 
 
 ;; getting events into riemann
@@ -24,6 +26,7 @@
   (take 10 (periodic-seq (t/now) (-> 1 t/seconds)))
   (fn [time]
     (riemann-send c {:service "dz0" :state "ok" :metric (rand 100)}))))
+
 ; chime-at returns a zero-arg function that can be called
 ; to cancel the schedule.
 ; (x)
@@ -31,4 +34,48 @@
 @(r/query c "service = \"dz0\"")
 
 
+; testing riemann-7.config
+@(r/query c "service = \"req rate\"")
+(map #(riemann-send c {:service "http req rate" :state "ok" :metric (rand %)}) [1 2 3 4 5])
+(riemann-send c {:service "0mq req rate" :state "ok" :metric (rand 100)})
+
+; testing riemann-8.config
+; this tests the 'project' stream. project takes multiple streams
+; as argument and allows users to project them into a single stream
+@(r/query c "service = \"mem percent\"")
+@(r/query c "service = \"mem used\"")
+@(r/query c "service = \"mem total\"")
+(def x (chime-at
+  (take 500 (periodic-seq (t/now) (-> 1 t/seconds)))
+  (fn [time]
+    (riemann-send c {:service "amem used" :state "ok" :metric (* 1.0 (rand 100))})
+    (riemann-send c {:service "amem total" :state "ok" :metric 100.0}))))
+
+;; testing coalesce
+(def x (chime-at
+  (take 100 (periodic-seq (t/now) (-> 1 t/seconds)))
+  (fn [time]
+    (riemann-send c {:service "disk xyz" :state "ok" :host "a" :metric 1.23})
+    (riemann-send c {:service "disk xyz" :state "ok" :host "b" :metric 2.77}))))
+;
+(def x (chime-at
+  (take 100 (periodic-seq (t/now) (-> 1 t/seconds)))
+  (fn [time]
+    (riemann-send c {:service "disk xyz" :state "ok" :host "a" :metric 1.23})
+    (riemann-send c {:service "disk xyz" :state "ok" :host "b" :metric 2.77}))))
+
+(def x (chime-at
+  (take 100 (periodic-seq (t/now) (-> 1 t/seconds)))
+  (fn [time]
+    (riemann-send c {:service "agent heartbeat" :state "ok" :host "a" :metric 1.23})
+    (riemann-send c {:service "agent heartbeat" :state "ok" :host "b" :metric 2.77}))))
+
+
+
+(let [chimes (chime-ch [(-> 2 t/secs t/from-now)
+                                                (-> 3 t/secs t/from-now)])]
+    (a/<!! (go-loop []
+                               (when-let [msg (<! chimes)]
+                                              (prn "Chiming at:" msg)
+                                                           (recur)))))
 
